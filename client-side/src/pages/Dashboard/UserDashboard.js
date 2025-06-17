bd7b2ec439fd77e25671563cefc16fb915bed906
Azizur Rahman <azizerorahman@gmail.com>
2025-06-18 03:27:36 +0800

    Refactor Dashboard and UserDashboard components for improved readability; remove unnecessary console logs and clean up code formatting

--- client-side/src/pages/Dashboard/UserDashboard.js
+++ client-side/src/pages/Dashboard/UserDashboard.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import {
